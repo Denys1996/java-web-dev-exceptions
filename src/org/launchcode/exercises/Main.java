1bce3fe7b0ea7fc4a20e16bb81d769a20d1e7ea0
denys1996 <dvchf26@gmail.com>
2022-08-16 18:43:34 -0400

    Finished exercise

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -7,25 +7,46 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
-
+        // Test out your divide() function!
+        //divide(40, 0);
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
+        studentFiles.put("Micho", "");
 
         // Test out your CheckFileExtension() function!
+        System.out.println(checkFileExtension(studentFiles.get("Micho")));
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void divide(int x, int y) {
+        if(y == 0) {
+           try{
+               throw new ArithmeticException("You can't divide by 0");
+           } catch (ArithmeticException ae) {
+               ae.printStackTrace();
+           }
+        } else {
+            System.out.println(x/y);
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int checkFileExtension(String fileName) {
+        int result = 0;
+        if(fileName == null || fileName.isEmpty()) {
+            try {
+                throw new NullPointerException("File cannot be null");
+            } catch (NullPointerException npe) {
+                npe.printStackTrace();
+                result = -1;
+                return result;
+            }
+        } else if (fileName.endsWith(".java")) {
+           result = 1;
+            return result;
+        }
+    return result;
     }
 
 }
